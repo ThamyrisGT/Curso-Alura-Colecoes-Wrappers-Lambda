43604905ef966dbd1071dfb59d161ecd2f36e5e8
Thamyris <tgthurler@gmail.com>
2022-01-19 12:32:01 -0300

    Ulitizando o linkedList

--- src/br/com/bytebank/banco/test/util/TesteArrayListEquals.java
+++ src/br/com/bytebank/banco/test/util/TesteArrayListEquals.java
@@ -1,7 +1,5 @@
 package br.com.bytebank.banco.test.util;
-
-import java.util.ArrayList;
-
+import java.util.LinkedList;
 
 import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
@@ -11,7 +9,7 @@ public class TesteArrayListEquals {
 	public static void main(String[] args) {
 
 		//Generics
-		ArrayList<Conta> lista = new ArrayList<Conta>();
+		LinkedList<Conta> lista = new LinkedList<Conta>();  // arrayList e LinkedList implementam a interface List
 		
 
 		Conta cc = new ContaCorrente(22, 11);
