f478e5575dbe58cc2be5809282bf732943a52db3
Thamyris <tgthurler@gmail.com>
2022-01-27 20:57:27 -0300

    Utilizando o objeto Iterator para iterar filas,conjuntos ou mapas

--- src/br/com/bytebank/banco/test/util/Teste.java
+++ src/br/com/bytebank/banco/test/util/Teste.java
@@ -2,8 +2,10 @@ package br.com.bytebank.banco.test.util;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
-import java.util.function.Consumer;
+import java.util.Set;
 
 import br.com.bytebank.banco.modelo.Cliente;
 import br.com.bytebank.banco.modelo.Conta;
@@ -65,6 +67,29 @@ public class Teste {
 //			}
 //			
 //		});
+		
+	
+		List<String> nomes = new ArrayList<>();
+		nomes.add("Super Mario");
+		nomes.add("Yoshi"); 
+		nomes.add("Donkey Kong"); 
+
+		Iterator<String> it = nomes.iterator();
+
+		while(it.hasNext()) {
+		  System.out.println(it.next());
+		}
+		
+		Set<String> nomes2 = new HashSet<>();
+		nomes2.add("Super Mario");
+		nomes2.add("Yoshi"); 
+		nomes2.add("Donkey Kong"); 
+
+		Iterator<String> it2 = nomes2.iterator();
+
+		while(it2.hasNext()) {
+		  System.out.println(it2.next());
+		}
 	}
 
 }
